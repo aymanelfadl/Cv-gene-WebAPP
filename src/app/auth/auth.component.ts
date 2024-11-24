194adc041716be90f1bee4e0539a6d951f6e465b
aymanelfadl <aymanelfadl16@gmail.com>
2024-11-24 17:19:50 +0100

    auth done

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,37 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
 })
 export class AuthComponent implements OnInit {
   isLoginMode: boolean = true;
   email: string = '';
   password: string = '';
+  firstName: string = '';
+  lastName: string = '';
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     this.route.url.subscribe((url) => {
-      this.isLoginMode = url[0]?.path === 'login';
+      this.isLoginMode = url[0]?.path === 'login'; // Switch mode based on the URL
     });
   }
 
   onSubmit(): void {
     if (this.isLoginMode) {
+      // Handle login
       console.log('Login with', this.email, this.password);
       // Perform login logic here
     } else {
-      console.log('Sign Up with', this.email, this.password);
+      console.log(
+        'Sign Up with',
+        this.firstName,
+        this.lastName,
+        this.email,
+        this.password
+      );
       // Perform sign-up logic here
     }
   }
-  // Switch between Login and Signup modes
+
   switchMode() {
     this.isLoginMode = !this.isLoginMode;
     const targetRoute = this.isLoginMode ? '/login' : '/signup';
