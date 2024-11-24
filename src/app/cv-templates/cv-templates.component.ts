9408032f8ab6e1eafdb13f33bc0e0ef50653a0b0
aymanelfadl <aymanelfadl16@gmail.com>
2024-11-24 18:30:40 +0100

    wtf

---
+++ src/app/cv-templates/cv-templates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-templates',
+  standalone: true,
+  imports: [],
+  templateUrl: './cv-templates.component.html',
+  styleUrl: './cv-templates.component.css'
+})
+export class CvTemplatesComponent {
+
+}
