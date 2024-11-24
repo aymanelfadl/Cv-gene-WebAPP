08673e83cc229b6197062cae227b50dab87b73c0
aymanelfadl <aymanelfadl16@gmail.com>
2024-11-24 18:24:54 +0100

    almost side bar ...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { SidenavComponent } from '../sidenav/sidenav.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SidenavComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidenav',
+  standalone: true,
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css'],
+  imports: [CommonModule],
+})
+export class SidenavComponent {}
