484c217078a04fa134a355e4d91792bcfecf20ec
aymanelfadl <aymanelfadl16@gmail.com>
2024-11-25 22:40:07 +0100

    haha

--- src/app/cv-templates/cv-templates.component.ts
+++ src/app/cv-templates/cv-templates.component.ts
@@ -1,18 +1,50 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cv-templates',
   templateUrl: './cv-templates.component.html',
   styleUrls: ['./cv-templates.component.css'],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   standalone: true,
 })
 export class CvTemplatesComponent {
   templates: string[] = ['1.jpg', '2.png', '2.png'];
-
   currentTemplateIndex: number = 0;
+  showPopup: boolean = false;
+  showNextPopup: boolean = false;
+  popupFormData = {
+    name: '',
+    email: '',
+    city: '',
+    linkdin: '',
+    data_birth: '',
+    country: '',
+  };
+  nextPopupFormData = {
+    university: '',
+    startYear: '',
+    endYear: '',
+    speciality: '',
+    description: '',
+    experience: '',
+  };
 
+  showNextForm() {
+    this.showPopup = false;
+    this.showNextPopup = true;
+  }
+
+  closeNextPopup() {
+    this.showNextPopup = false;
+  }
+
+  onSubmitNextPopupForm() {
+    // Handle next form submission logic here
+    console.log('Next form submitted:', this.nextPopupFormData);
+    this.closeNextPopup();
+  }
   autoScroll() {
     setInterval(() => {
       if (this.currentTemplateIndex < this.templates.length - 1) {
@@ -25,5 +57,19 @@ export class CvTemplatesComponent {
 
   ngOnInit() {
     this.autoScroll();
+  }
+
+  openPopup() {
+    this.showPopup = true;
+  }
+
+  closePopup() {
+    this.showPopup = false;
+  }
+
+  onSubmitPopupForm() {
+    // Handle form submission logic here
+    console.log('Form submitted:', this.popupFormData);
+    this.closePopup();
   }
 }
