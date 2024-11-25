e2f1427993415fc0b1827b8dcdcaa0ac10a8fa6f
aymanelfadl <aymanelfadl16@gmail.com>
2024-11-25 20:16:37 +0100

    ok hh

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CvTemplatesComponent } from '../cv-templates/cv-templates.component';
-import { HeaderComponent } from '../header/header.component';
+
 @Component({
   selector: 'app-sidenav',
   standalone: true,
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css'],
-  imports: [CommonModule, CvTemplatesComponent, HeaderComponent],
+  imports: [CommonModule, CvTemplatesComponent],
 })
 export class SidenavComponent {}
