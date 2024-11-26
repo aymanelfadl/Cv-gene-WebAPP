dfcae6d903e336f3b3a790dea874c2ef1656a72a
aymanelfadl <aymanelfadl16@gmail.com>
2024-11-26 13:22:02 +0100

    heyh

--- src/app/cv-templates/cv-templates.component.ts
+++ src/app/cv-templates/cv-templates.component.ts
@@ -12,39 +12,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 export class CvTemplatesComponent {
   templates: string[] = ['1.jpg', '2.png', '2.png'];
   currentTemplateIndex: number = 0;
-  showPopup: boolean = false;
-  showNextPopup: boolean = false;
-  popupFormData = {
-    name: '',
-    email: '',
-    city: '',
-    linkdin: '',
-    data_birth: '',
-    country: '',
-  };
-  nextPopupFormData = {
-    university: '',
-    startYear: '',
-    endYear: '',
-    speciality: '',
-    description: '',
-    experience: '',
-  };
 
-  showNextForm() {
-    this.showPopup = false;
-    this.showNextPopup = true;
-  }
-
-  closeNextPopup() {
-    this.showNextPopup = false;
-  }
-
-  onSubmitNextPopupForm() {
-    // Handle next form submission logic here
-    console.log('Next form submitted:', this.nextPopupFormData);
-    this.closeNextPopup();
-  }
   autoScroll() {
     setInterval(() => {
       if (this.currentTemplateIndex < this.templates.length - 1) {
@@ -57,19 +25,5 @@ export class CvTemplatesComponent {
 
   ngOnInit() {
     this.autoScroll();
-  }
-
-  openPopup() {
-    this.showPopup = true;
-  }
-
-  closePopup() {
-    this.showPopup = false;
-  }
-
-  onSubmitPopupForm() {
-    // Handle form submission logic here
-    console.log('Form submitted:', this.popupFormData);
-    this.closePopup();
   }
 }
